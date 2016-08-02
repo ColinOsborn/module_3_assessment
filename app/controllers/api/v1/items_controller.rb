de41008583b3e43abbf8f72959711a0be38bcb58
Colin Osborn <colin.osborn@gmail.com>
2016-08-02 10:02:12 -0600

    moving on to api consumption

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,9 +1,8 @@
-class Api::V1::Item::ItemsController < ApplicationController
+class Api::V1::Items::ItemsController < ApplicationController
   respond_to :json
 
   def index
     respond_with Items.all
   end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root 'items#index'
 
+  namespace :api do
+    namespace :v1, defaults: {format: :json } do
+      resources :items, only: [:show, :index]
+    end
+  end
+
   resources :items,  only: [:index, :show]
   resources :orders, only: [:index, :show]
   resources :users,  only: [:index, :show]
 
-  namespace :api do
-    namespace :v1, defaults: {format: :json } do
-      resources :items, only: [:show, :index]
-      
-      end
-    end
 end
